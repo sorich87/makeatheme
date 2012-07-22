6f8ade58a11d7e760df8262cea76140058fa04ef
Ulrich Sossou <sorich87@gmail.com>
2012-07-22 10:17:37 +0100

    Added menu edit mode

--- js/views/menu.js
+++ js/views/menu.js
@@ -2,17 +2,30 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'views/base',
   'collections/menu',
   'views/menu_item'
-  ], function($, _, Backbone, Menu, MenuItemView) {
-  var MenuView = Backbone.View.extend({
+  ], function($, _, Backbone, BaseView, Menu, MenuItemView) {
+  var MenuView = BaseView.extend({
       el: $("<ul></ul>").appendTo($('#theme iframe').contents().find(".x-menu"))
 
-    , initialize: function () {
+    , initialize: function (options) {
+      this.constructor.__super__.initialize.apply(this, [options])
+    }
+
+    , loadModel: function () {
+      Menu.reset([{name: "Page", url: "#"}, {name: "Another Page", url: "#"}]);
+    }
+
+    , switchModes: function () {
+      EventDispatcher.on("mode:edit", function () {
+        this.$el.append("<li class='x-edit'><a style='background-color: #ffffe0; color: #1982d1;'>Add Page</a></li>");
+      }, this);
+    }
+
+    , saveChanges: function () {
       Menu.on('add',   this.addOne, this);
       Menu.on('reset', this.addAll, this);
-
-      Menu.reset([{name: "Page", url: "#"}, {name: "Another Page", url: "#"}]);
     }
 
     , addOne: function(menu_item) {
