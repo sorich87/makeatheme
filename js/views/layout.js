278408a319edfbab8fae7d29b068d2d0ba94cc69
Ulrich Sossou <sorich87@gmail.com>
2012-07-28 15:29:26 +0100

    Some refactoring Removed drag.scroll plugin

--- js/views/layout.js
+++ js/views/layout.js
@@ -22,56 +22,66 @@ define([
         require([
                 "jquerypp/event/drag",
                 "jquerypp/event/drag.limit",
-                "jquerypp/event/drag.scroll",
                 "jquerypp/event/drop"
         ], function () {
           // Links in draggable areas shouldn't be clickable
           _this.$(".columns a").on("click", preventDefault);
 
-          _this.$el.on("draginit", ".columns", function (ev, drag) {
-            drag.limit($("body").children());
-            drag.scrolls($("iframe", parent.document));
-          });
-
-          // Cancel drag on content editable areas to allow edit
-          _this.$el.on("dragdown", ".columns", function (ev, drag) {
-            if ($(ev.target).attr("contenteditable") === true || $(ev.target).hasClass("x-edit")) {
-              drag.cancel();
+          _this.$el.on({
+            draginit: function (ev, drag) {
+              // Limit drag to first container
+              drag.limit($("body").children());
             }
-          });
 
-          _this.$el.on("dropon", ".row", function (ev, drop, drag) {
-            var parentWidth, childrenWidth, dragParent;
-
-            parentWidth = $(this).width();
-
-            childrenWidth = _.reduce($(this).children(), function (memo, child) {
-              if ($(child).is(drag.element)) {
-                return memo;
-              } else {
-                return memo + parseFloat($(child).outerWidth());
+            , dragdown: function (ev, drag) {
+              // Cancel drag on editable areas to allow edit
+              if ($(ev.target).attr("contenteditable") === true || $(ev.target).hasClass("x-edit")) {
+                drag.cancel();
               }
-            }, 0, this);
-
-            $dragParent = $(drag.element).parent();
-
-            if ((parentWidth - childrenWidth) > (parentWidth * 8.333 / 100)) {
-              row = this;
-            } else {
-              row = $("<div class='row'></div>").insertAfter(this);
             }
+          }, ".columns");
 
-            $(drag.element).appendTo(row).css({top: 0, left: 0});
+          _this.$el.on({
+            dropon: function (ev, drop, drag) {
+              var rowWidth, totalColumnsWidth, dragParent;
 
-            if ($dragParent.children().length <= 0) {
-              $dragParent.remove();
+              // Width of the drop row
+              rowWidth = $(this).width();
+
+              // Total width of all columns children of the drop row
+              totalColumnsWidth = _.reduce($(this).children(), function (memo, child) {
+                if ($(child).is(drag.element)) {
+                  return memo;
+                } else {
+                  return memo + parseFloat($(child).outerWidth());
+                }
+              }, 0, this);
+
+              // Add column to row. If the row is full, add a new one
+              if ((rowWidth - totalColumnsWidth) >= (rowWidth * 8.333 / 100)) {
+                row = this;
+              } else {
+                row = $("<div class='row'></div>").insertAfter(this);
+              }
+
+              // Save original parent then append the column to drop row
+              dragParent = $(drag.element).parent();
+
+              $(drag.element).appendTo(row).css({top: 0, left: 0});
+
+              // If original parent doesn't have any more children,
+              // remove it
+              if ($(dragParent).children().length <= 0) {
+                $(dragParent).remove();
+              }
             }
-          });
+          }, ".row");
         });
       });
 
       EventDispatcher.on("mode:view", function () {
         require(["jquerypp/event/drag"], function () {
+          // Unbind drag events
           _this.$el.off("draginit", ".columns");
           _this.$el.off("dragdown", ".columns");
 
