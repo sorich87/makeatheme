0ed091432c1ab6a9cb50175a9e9fe6bd53dc5e46
Ulrich Sossou <sorich87@gmail.com>
2012-08-02 20:34:37 +0100

    Added login and register modals

--- js/index.js
+++ js/index.js
@@ -1,8 +1,16 @@
 require(["jquery"], function($) {
   $(function () {
     require(["bootstrap/js/bootstrap-collapse"], function () {
+      // Show FAQ on index, collapsed by default
       $(".collapse").collapse();
       $("[href='#faq']").on("click", function (e) { e.preventDefault() });
+    });
+
+    require(["bootstrap/js/bootstrap-modal"], function () {
+      // Hide modal previously shown before showing a new one
+      $("#register, #login, #new-password, #confirm-password").on("show", function () {
+        $("#register, #login, #new-password, #confirm-password").not("#" + this.id).modal("hide");
+      })
     });
   });
 });
