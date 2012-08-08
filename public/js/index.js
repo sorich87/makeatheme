4c7b6b19d11ea3bdbfa4b73b6a79d852687a484c
Emil Ahlbäck <e.ahlback@gmail.com>
2012-08-08 09:47:50 +0300

    Adding support for data-method="<method>"

--- public/js/index.js
+++ public/js/index.js
@@ -12,5 +12,20 @@ require(["jquery"], function($) {
         $("#register, #login, #new-password, #confirm-password").not("#" + this.id).modal("hide");
       })
     });
+
+    $('[data-method]').click(function() {
+      // TODO: Add "Are you sure?" if link has data-confirm = true
+      var f = document.createElement('form');
+      var method = $(this).attr('data-method').toLowerCase();
+      $(this).after($(f).attr({
+        method: 'post',
+        action: $(this).attr('href'),
+        style: "visibility:hidden;display:none;"
+      }).append('<input type="hidden" name="_method" value="'
+          + method
+          + '" />'));
+      $(f).submit();
+      return false;
+    })
   });
 });
