f022e192738a962d94fc7d80cec1dba1062735e7
Ulrich Sossou <sorich87@gmail.com>
2012-08-13 12:58:58 +0100

    Removed unused files

--- public/js/models/block.js
+++
@@ -1,5 +0,0 @@
-define(["underscore", "backbone"], function (_, Backbone) {
-	var BlockModel = Backbone.Model.extend();
-
-	return BlockModel;
-});
--- public/js/models/region.js
+++
@@ -1,6 +0,0 @@
-define(["underscore", "backbone"], function (_, Backbone) {
-	var RegionModel = Backbone.Model.extend();
-
-	return RegionModel;
-});
-
