0a76a63ddfb33b76567920087f2812af0b46fd54
Emil Ahlbäck <e.ahlback@gmail.com>
2012-08-15 10:20:28 +0300

    Simple Theme model. Define as_json for Theme, include pagination information in JSON response for /themes.json

--- app.rb
+++ app.rb
@@ -43,6 +43,13 @@ helpers do
     themes = Theme.order_by([:name, :desc]).page(params[:page])
     erb :index, :locals => {:themes => themes}
   end
+
+  def json_pagination_for(model)
+    {
+      per_page: model.default_per_page,
+      total_results: model.total_count
+    }
+  end
 end
 
 get '/' do
@@ -50,7 +57,11 @@ get '/' do
 end
 
 get '/themes.json' do
-  Theme.all.order_by([:name, :desc]).page(params[:page]).to_json
+  @themes = Theme.all.order_by([:name, :desc]).page(params[:page])
+  {
+    results: @themes,
+    pagination: json_pagination_for(@themes)
+  }.to_json
 end
 
 get '/themes/:id.json' do
--- models/theme.rb
+++ models/theme.rb
@@ -1,2 +1,37 @@
-class Theme < Extension
+require 'paperclip'
+require 'fog'
+
+class Theme
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  include Paperclip::Glue
+
+  paginates_per 16
+
+  field :name,        type: String
+  field :author,      type: String
+  field :author_uri,  type: String
+
+  # Fields used by Paperclip
+  field :screenshot_file_name
+  field :screenshot_content_type
+  field :screenshot_file_size,    :type => Integer
+  field :screenshot_updated_at,   :type => DateTime
+
+  validates_presence_of [:name, :author]
+
+  def as_json(options={})
+    {
+      :name => self.name,
+      :author => self.author,
+      :author_uri => self.author_uri,
+      :screenshot_uri => self.screenshot.url
+    }
+  end
+
+  has_attached_file :screenshot,
+    styles: { thumb: '320x240>' },
+    fog_public: true,
+    path: 'extensions/:attachment/:id/:style/:filename'
 end
+
