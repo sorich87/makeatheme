e35c94f7af9815a1ddd8f19f713d0d10c1082458
Ulrich Sossou <sorich87@gmail.com>
2012-08-28 11:20:25 +0100

    Changed AWS details in Paperclip initializer

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -4,8 +4,8 @@ require 'pathname' # Because Paperclip doesn't require it and fails
 # TODO: Find out why giving it the fog.yml file fails.
 Paperclip::Attachment.default_options[:storage] = :fog
 Paperclip::Attachment.default_options[:fog_credentials] = {
-  aws_access_key_id: 'AKIAJJWCERXLBPMNAGGA',
-  aws_secret_access_key: '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',
+  aws_access_key_id: 'AKIAJ7EH5OFSFNYHXQWA',
+  aws_secret_access_key: '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',
   provider: 'AWS'
 }
 Paperclip::Attachment.default_options[:fog_directory] = 'thememy-test'
