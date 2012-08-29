78b63aed992657eb257cdbe55dbbd34d39dcfa4b
Emil Ahlbäck <e.ahlback@gmail.com>
2012-08-29 13:58:17 +0300

    Add original_files for file group & add specs

--- models/theme_file_group.rb
+++ models/theme_file_group.rb
@@ -3,11 +3,24 @@ require 'static_theme_file'
 class ThemeFileGroup
   include Mongoid::Document
 
+  field :original_file_ids, type: Array
+
   has_many :static_theme_files
   has_many :themes
+
+  before_create :add_original_files
 
   def static_files_dir
     s = self.static_theme_files.first
     s.file.url.split(s.file_name).first[0..-2]
+  end
+
+  def original_files
+    self.static_theme_files.criteria.for_ids(self.original_file_ids)
+  end
+
+  private
+  def add_original_files
+    self.original_file_ids = self.static_theme_file_ids
   end
 end
--- spec/models/theme_file_group.rb
+++ spec/models/theme_file_group.rb
@@ -1,5 +1,27 @@
 describe ThemeFileGroup do
-
-  it { should embed_many(:static_theme_files) }
+  it { should have_many(:static_theme_files) }
   it { should have_many(:themes) }
+  it { should respond_to(:original_files) }
+
+  describe 'original files' do
+    before do
+      @valid_theme_zip = File.join('.', 'spec/fixtures/themes', 'basic_valid_theme.zip')
+      @valid_attributes = {
+        :name => "Some name",
+        :description => "Description of some theme",
+        :author => StoreUser.first,
+      }
+      @theme = Theme.create_from_zip(@valid_theme_zip, @valid_attributes)
+      @theme.save
+      @group = @theme.theme_file_group
+    end
+
+    specify "should be the same as the Theme's static files" do
+      @group.original_file_ids.should == @theme.static_theme_file_ids
+    end
+
+    specify "should return instances of the static files" do
+      @group.original_files.should == @theme.static_theme_files
+    end
+  end
 end
