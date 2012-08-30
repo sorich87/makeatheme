455b4a416bf670ccb2be6208d0b28c2cb2d7ad11
Ulrich Sossou <sorich87@gmail.com>
2012-08-30 23:14:34 +0100

    Fixed download button view

--- client/app/views/download_button.js
+++ client/app/views/download_button.js
@@ -1,31 +1,39 @@
 var View = require("views/base/view")
+  , Regions = require("collections/regions")
+  , Templates = require("collections/templates")
   , app = require("application");
 
 module.exports = View.extend({
     id: "x-download-button"
 
-  , render: function () {
-    this.$el.html("<button class='x-btn x-btn-success'>Download Theme</button>");
+  , events: {
+    "click button": "download"
+  }
 
-    $(window.document).on("click", "button", this.download);
+  , initialize: function () {
+    this.regions = new Regions(app.data.theme_pieces.regions);
+    this.templates = new Templates(app.data.theme_pieces.templates);
+  }
+
+  , render: function () {
+    this.$el.empty().append("<button class='x-btn x-btn-success'>Download Theme</button>");
 
     return this;
   }
 
   , download: function () {
     var customization = {
-        regions: app.regions.models
-      , templates: app.templates.models
+        regions: this.regions.models
+      , templates: this.templates.models
     };
 
     $.ajax({
-        url: "/themes/" + window.parent.themeID + "/customize.json"
+        url: "/themes/" + app.data.theme._id + "/customize.json"
       , type: "POST"
       , contentType: "application/json; charset=utf-8"
       , data: JSON.stringify(customization)
-      , success: function(data) {
-          var el = '<iframe src="data:application.zip/octet-stream;base64,'+data+'"></iframe>.';
-          $('body').append($(el));
+      , success: function(theme) {
+        window.open(theme.archive, "_blank");
       }
     });
   }
