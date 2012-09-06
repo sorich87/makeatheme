42adce343db706c76992e795f6f4fa3f2eb33904
Ulrich Sossou <sorich87@gmail.com>
2012-09-06 09:11:00 +0100

    Check for archive file existence before trying to add it to theme JSON

--- models/theme.rb
+++ models/theme.rb
@@ -81,7 +81,7 @@ class Theme
       :name => self.name,
       :author => self.author.to_fullname,
       :screenshot_uri => self.screenshot.url,
-      :archive => self.archive.expiring_url
+      :archive => if self.archive.file? then self.archive.expiring_url else nil end
     }
   end
 
