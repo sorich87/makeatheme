cdbb4948794f36f5a450320fe8ecc2abd21de996
Ulrich Sossou <sorich87@gmail.com>
2012-09-13 11:36:36 +0100

    Correct use partial for searchCorrect use partial for search form in theme archive

--- lib/defaults.rb
+++ lib/defaults.rb
@@ -131,11 +131,7 @@ module Defaults
 
 <?php endif; ?>),
 
-      search_form: %q(<form method="get" id="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
-  <label for="s" class="assistive-text"><?php _e( 'Search', '_s' ); ?></label>
-  <input type="text" class="field" name="s" id="s" placeholder="<?php esc_attr_e( 'Search &hellip;', '_s' ); ?>" />
-  <input type="submit" class="submit" name="submit" id="searchsubmit" value="<?php esc_attr_e( 'Search', '_s' ); ?>" />
-</form>),
+      search_form: %q(<?php get_search_form(); ?>),
 
       header_image: %q(<?php $header_image = get_header_image();
 if ( ! empty( $header_image ) ) : ?>
