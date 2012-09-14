sorich87/makeatheme

commit e518779d492a3685595aa268ce8b476c1044edaf

Render different article block per template in theme archive

lib/theme_archive.rb
@@ -62,8 +62,12 @@ module ThemeArchive
             footer = "<?php get_footer('#{template.regions[:footer]}'); ?>"
           end
 
-          template = header + template[:template] + footer
-          f.puts render_template(template, @base)
+          content = template[:template]
+          if %w(index single page).include?(template.name)
+            content.gsub!(/\{\{(.+?)\}\}/) { "{{#{$1.strip}-#{template.slug}}}" }
+          end
+
+          f.puts render_template(header + content + footer, @base)
         end
       end
     end

lib/defaults.rb
@@ -107,7 +107,7 @@ module Defaults
       site_title: "<?php bloginfo( 'name' ); ?>",
       site_description: "<?php bloginfo( 'description' ); ?>",
 
-      article: %q(<?php if ( have_posts() ) : ?>
+      "article-index" => %q(<?php if ( have_posts() ) : ?>
 <?php _s_content_nav( 'nav-above' ); ?>
 
 <?php /* Start the Loop */ ?>
@@ -128,6 +128,49 @@ module Defaults
 <?php elseif ( current_user_can( 'edit_posts' ) ) : ?>
 
 <?php get_template_part( 'no-results', 'index' ); ?>
+
+<?php endif; ?>),
+
+      "article-single" => %q(<?php while ( have_posts() ) : the_post(); ?>
+
+  <?php _s_content_nav( 'nav-above' ); ?>
+
+  <?php get_template_part( 'content', 'single' ); ?>
+
+  <?php _s_content_nav( 'nav-below' ); ?>
+
+  <?php
+    // If comments are open or we have at least one comment, load up the comment template
+    if ( comments_open() || '0' != get_comments_number() )
+      comments_template( '', true );
+  ?>
+
+<?php endwhile; // end of the loop. ?>),
+
+      "article-page" => %q(<?php while ( have_posts() ) : the_post(); ?>
+
+  <?php get_template_part( 'content', 'page' ); ?>
+
+  <?php comments_template( '', true ); ?>
+
+<?php endwhile; // end of the loop. ?>),
+
+      article: %q(<?php if ( have_posts() ) : ?>
+
+  <?php _s_content_nav( 'nav-above' ); ?>
+
+  <?php /* Start the Loop */ ?>
+  <?php while ( have_posts() ) : the_post(); ?>
+
+    <?php get_template_part( 'content' ); ?>
+
+  <?php endwhile; ?>
+
+  <?php _s_content_nav( 'nav-below' ); ?>
+
+<?php else : ?>
+
+  <?php get_template_part( 'no-results' ); ?>
 
 <?php endif; ?>),
 

models/template.rb
@@ -11,8 +11,12 @@ class Template
     self[:regions].symbolize_keys
   end
 
+  def slug
+    self[:name].parameterize
+  end
+
   # PHP filename
   def filename
-    "#{self[:name].parameterize}.php"
+    "#{self.slug}.php"
   end
 end

commit a15d82ab04a5388d9a6586fa7c58eb393c4eb812

Fixed bug introduced by e518779 where Liquid tags other than {{article}} are stripped from templates

lib/theme_archive.rb
@@ -65,7 +65,10 @@ module ThemeArchive
           # convert 'article' tag to 'article-slug' for index, single and page templates
           content = template[:template]
           if %w(index single page).include?(template.name)
-            content.gsub!(/\{\{(.+?)\}\}/) { "{{#{$1.strip}-#{template.slug}}}" }
+            content.gsub!(/\{\{(.+?)\}\}/) do |match|
+              tag = $1.strip
+              if tag == 'article' then "{{#{tag}-#{template.slug}}}" else match end
+            end
           end
 
           # Add template name comment before header if template is not a default one.