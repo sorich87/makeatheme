c1a460d82a6f818d9b8a73ca5d0ace259c8c4cb8
Emil Ahlbäck <e.ahlback@gmail.com>
2012-09-14 17:51:32 +0300

    Basic (broken redirect!) notification of success in successful upload

--- client/app/views/theme_upload.js
+++ client/app/views/theme_upload.js
@@ -1,4 +1,5 @@
-var View = require("views/base/view");
+var View = require("views/base/view")
+  , app = require("application");;
 
 module.exports = View.extend({
     className: "modal"
@@ -17,11 +18,17 @@ module.exports = View.extend({
       , url: "/themes"
       , data: new FormData(this.$("form")[0])
       , success: function(data, textStatus, jqXHR) {
-        console.log(data, textStatus, jqXHR);
-      }
+        this.$el.modal("hide");
+        app.trigger("notification", "success", "Your theme is uploaded and ready to be customized!");
+        // TODO: This is so wrong I want to cry.
+        window.location = "/themes/" + data._id;
+      }.bind(this)
+
       , error: function(jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR, textStatus, errorThrown);
-      }
+        this.$el.modal("hide");
+        app.trigger("notification", "error", "The theme you provided was not valid.");
+      }.bind(this)
+
       , cache: false
       , contentType: false
       , processData: false
