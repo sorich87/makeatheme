96093357811f83f2f7bdd6efc45c167c3235ee74
Ulrich Sossou <sorich87@gmail.com>
2012-09-23 16:36:16 +0100

    Per user session handling of server events

--- helpers/application_helper.rb
+++ helpers/application_helper.rb
@@ -14,14 +14,16 @@ module ApplicationHelper
     }
   end
 
-  def send_event(name, args)
+  def send_event(name, args, session_id)
     args.collect! {|arg| "\"#{arg}\""}
     data = "data: {\n"
     data << "data: \"name\": \"#{name}\",\n"
     data << "data: \"args\": [#{args.join(', ')}]\n"
     data << "data: }\n\n"
 
-    settings.connections.each do |out|
+    settings.connections[session_id] = [] if settings.connections[session_id].nil?
+
+    settings.connections[session_id].each do |out|
       out << data
     end
   end
--- routes/application_routes.rb
+++ routes/application_routes.rb
@@ -1,7 +1,12 @@
 
 get '/events', provides: 'text/event-stream' do
+  session_start!
+  session_id = session['session_id']
+
   stream :keep_open do |out|
-    settings.connections << out
-    out.callback { settings.connections.delete(out) }
+    settings.connections[session_id] = [] if settings.connections[session_id].nil?
+
+    settings.connections[session_id] << out
+    out.callback { settings.connections[session_id].delete(out) }
   end
 end
