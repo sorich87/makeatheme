00e0c858ef0f8f0845a091b5c4ec74bf1cf90297
Ulrich Sossou <sorich87@gmail.com>
2012-09-30 19:33:39 +0100

    Added redistogo for production

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,1 +1,6 @@
+if production?
+  uri = URI.parse(ENV["REDISTOGO_URL"])
+  REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
+end
+
 Resque::Plugins::Status::Hash.expire_in = (24 * 60 * 60)
