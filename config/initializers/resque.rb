1a002f960d26859a993b081f034f90f59bbc7624
Ulrich Sossou <sorich87@gmail.com>
2012-11-09 07:29:54 +0100

    Update Resque production settings

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,2 +1,19 @@
-Resque.redis = ENV["REDISTOGO_URL"] if production?
+if production?
+  if ENV['REDIS_PASSWORD'].nil?
+    services = JSON.parse(ENV['VCAP_SERVICES'])
+    redis_key = services.keys.select { |svc| svc =~ /redis/i }.first
+    redis = services[redis_key].first['credentials']
+    Resque.redis = Redis.new(
+      host: redis['hostname'],
+      port: redis['port'],
+      password: redis['password']
+    )
+  else
+    Resque.redis = Redis.new(
+      host: 'localhost',
+      port: 10000,
+      password: ENV['REDIS_PASSWORD']
+    )
+  end
+end
 Resque::Plugins::Status::Hash.expire_in = (24 * 60 * 60)
