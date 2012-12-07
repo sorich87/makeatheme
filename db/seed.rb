787fb28fcc8225342d6713bfafcd3ecc5fbaede7
Ulrich Sossou <sorich87@gmail.com>
2012-12-07 11:42:55 +0100

    Improve seed script

--- db/seed.rb
+++ db/seed.rb
@@ -10,36 +10,27 @@ unless author
 end
 
 Dir[File.join(File.dirname(__FILE__), 'themes', '*.zip')].each do |theme_file|
-  attrs = [
-    {
-      author: author,
-      listed: true
-    }
-  ]
+  new_theme = Theme.new_from_zip(theme_file, author: author, listed: true)
 
-  attrs.each do |theme_attr|
-    new_theme = Theme.new_from_zip(theme_file, theme_attr)
-
-    theme = Theme.where(name: new_theme.name, author_id: author.id).first
-    if theme.nil?
-      theme = new_theme
-    else
-      theme.write_attributes(new_theme.attributes)
-      new_theme.assets.each do |asset|
-        theme.assets << asset
-      end
-      theme.blocks = new_theme.blocks
-      theme.regions = new_theme.regions
-      theme.templates = new_theme.templates
+  theme = Theme.where(name: new_theme.name, author_id: author.id).first
+  if theme.nil?
+    theme = new_theme
+  else
+    theme.write_attributes(new_theme.attributes)
+    new_theme.assets.each do |asset|
+      theme.assets << asset
     end
+    theme.blocks = new_theme.blocks
+    theme.regions = new_theme.regions
+    theme.templates = new_theme.templates
+  end
 
-    if theme.save
-      Jobs::ThemeArchive.perform_async(theme.id)
-    else
-      puts "Errors...:"
-      theme.errors.each do |key, fault|
-        puts "\t#{key} #{fault}"
-      end
+  if theme.save
+    Jobs::ThemeArchive.perform_async(theme.id)
+  else
+    puts "Errors...:"
+    theme.errors.each do |key, fault|
+      puts "\t#{key} #{fault}"
     end
   end
 end
