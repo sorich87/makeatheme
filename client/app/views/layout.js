sorich87/makeatheme

commit 73bdb77f1149000cae7dcfdeaf11931a5c2925b6

Don't remove last empty row in content and highlight it

client/app/views/layout.js
@@ -269,11 +269,13 @@ module.exports = View.extend({
          parent.children.length === 1) ||
         (node.id.indexOf("x-") !== 0 && node.id.indexOf("y-") !== 0)) {
       node.className += " x-empty";
-    } else {
+    } else if (parent.children.length !== 1) {
       parent.removeChild(node);
 
       app.trigger("node:removed", node,
                   window.document.getElementById(parentId), "row");
     }
+
+    this.highlightEmpty();
   }
 });

public/javascripts/app.js
@@ -3178,12 +3178,14 @@ window.require.define({"views/layout": function(exports, require, module) {
            parent.children.length === 1) ||
           (node.id.indexOf("x-") !== 0 && node.id.indexOf("y-") !== 0)) {
         node.className += " x-empty";
-      } else {
+      } else if (parent.children.length !== 1) {
         parent.removeChild(node);
 
         app.trigger("node:removed", node,
                     window.document.getElementById(parentId), "row");
       }
+
+      this.highlightEmpty();
     }
   });
   

commit 818c244328d425b8bc49a464975a8a879a4981c5

Fix row removal bug introduced in 73bdb77f1149000cae7dcfdeaf11931a5c2925b6

client/app/views/layout.js
@@ -265,17 +265,14 @@ module.exports = View.extend({
     var parent = node.parentNode
       , parentId = parent.id;
 
-    if ((["HEADER", "FOOTER"].indexOf(parent.tagName) !== -1 &&
-         parent.children.length === 1) ||
+    if (parent.children.length === 1 ||
         (node.id.indexOf("x-") !== 0 && node.id.indexOf("y-") !== 0)) {
       node.className += " x-empty";
-    } else if (parent.children.length !== 1) {
+    } else {
       parent.removeChild(node);
 
       app.trigger("node:removed", node,
                   window.document.getElementById(parentId), "row");
     }
-
-    this.highlightEmpty();
   }
 });

public/javascripts/app.js
@@ -3177,18 +3177,15 @@ window.require.define({"views/layout": function(exports, require, module) {
       var parent = node.parentNode
         , parentId = parent.id;
 
-      if ((["HEADER", "FOOTER"].indexOf(parent.tagName) !== -1 &&
-           parent.children.length === 1) ||
+      if (parent.children.length === 1 ||
           (node.id.indexOf("x-") !== 0 && node.id.indexOf("y-") !== 0)) {
         node.className += " x-empty";
-      } else if (parent.children.length !== 1) {
+      } else {
         parent.removeChild(node);
 
         app.trigger("node:removed", node,
                     window.document.getElementById(parentId), "row");
       }
-
-      this.highlightEmpty();
     }
   });
   