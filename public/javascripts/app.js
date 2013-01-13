0c7e88bfa9094a8b4459b49d1a7b6545a1f31ad8
Ulrich Sossou <sorich87@gmail.com>
2013-01-13 13:38:50 +0100

    Fix user update and delete routes

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1650,7 +1650,7 @@ window.require.define({"models/user": function(exports, require, module) {
       var collection = new Themes(this.attributes.themes);
     }
 
-    , url: "/users"
+    , urlRoot: "/users"
 
     , validation: {
       first_name: {
