0f6bfb39272009bdd62ebf7a17256b935794c960
Ulrich Sossou <sorich87@gmail.com>
2013-01-21 08:10:34 +0100

    Reorder theme routes

--- routes/theme_routes.rb
+++ routes/theme_routes.rb
@@ -21,6 +21,24 @@ post '/themes' do
   halt copy.to_json
 end
 
+put '/themes/:id' do
+  protect!
+
+  halt 401 unless theme.author?(current_user)
+
+  params = JSON.parse(request.body.read, symbolize_names: true)
+
+  respond_with_saved_theme!(theme, params)
+end
+
+delete '/themes/:id' do
+  protect!
+
+  halt 401 unless theme.author?(current_user)
+
+  theme.destroy
+end
+
 get '/themes/:id/edit', provides: 'html' do
   respond_with_editor!
 end
@@ -39,20 +57,3 @@ get '/themes/:id/download' do
   redirect archive.expiring_url
 end
 
-put '/themes/:id' do
-  protect!
-
-  halt 401 unless theme.author?(current_user)
-
-  params = JSON.parse(request.body.read, symbolize_names: true)
-
-  respond_with_saved_theme!(theme, params)
-end
-
-delete '/themes/:id' do
-  protect!
-
-  halt 401 unless theme.author?(current_user)
-
-  theme.destroy
-end
