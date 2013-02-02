910dd18681b091a2b95d1035080dcc792b51dee3
Ulrich Sossou <sorich87@gmail.com>
2013-02-02 07:54:11 +0100

    Fix error where window.top is undefined

--- client/app/initialize.js
+++ client/app/initialize.js
@@ -1,40 +1,45 @@
 var app = require('application');
 
-jQuery(function($) {
-  app.initialize();
+(function ($, window) {
+  $(function() {
+    var top = window.top;
 
-  // Enable HTML5 pushstate
-  Backbone.history.start({pushState: true});
+    app.initialize();
 
-  // All navigation that is relative should be passed through the navigate
-  // method, to be processed by the router if the user is not on the homepage.
-  // If the link has a `data-bypass` attribute, bypass the delegation completely.
-  // If the link has a `data-replace` attribute, update the URL without creating
-  // an entry in the browser history.
-  $(window.top.document).on("click", "a:not([data-bypass])", function(e) {
-    var href = { prop: $(this).prop("href"), attr: $(this).attr("href") }
-    , root = window.top.location.protocol + "//" + window.top.location.host + "/";
+    // Enable HTML5 pushstate
+    Backbone.history.start({pushState: true});
 
-    if (href.attr === "#") {
-      return;
-    }
+    // All navigation that is relative should be passed through the navigate
+    // method, to be processed by the router if the user is not on the homepage.
+    // If the link has a `data-bypass` attribute, bypass the delegation completely.
+    // If the link has a `data-replace` attribute, update the URL without creating
+    // an entry in the browser history.
+    $(top.document).on("click", "a:not([data-bypass])", function(e) {
+      var href = { prop: $(this).prop("href"), attr: $(this).attr("href") }
+      , root = top.location.protocol + "//" + top.location.host + "/";
 
-    if (href.prop && href.prop.slice(0, root.length) === root &&
-       Backbone.history.fragment !== "") {
-      e.preventDefault();
+      if (href.attr === "#") {
+        return;
+      }
 
-      Backbone.history.navigate(href.attr, {
-        trigger: true,
-        replace: !!$(this).data("replace")
-      });
-    }
+      if (href.prop && href.prop.slice(0, root.length) === root &&
+         Backbone.history.fragment !== "") {
+        e.preventDefault();
+
+        Backbone.history.navigate(href.attr, {
+          trigger: true,
+          replace: !!$(this).data("replace")
+        });
+      }
+    });
+
+    // Google Analytics
+    Backbone.history.on("route", function (name, args) {
+      if ("_gaq" in window) {
+        var url = "/" + this.getFragment();
+        _gaq.push(["_trackPageview", url]);
+      }
+    });
   });
+})(jQuery, window);
 
-  // Google Analytics
-  Backbone.history.on("route", function (name, args) {
-    if ("_gaq" in window) {
-      var url = "/" + this.getFragment();
-      _gaq.push(["_trackPageview", url]);
-    }
-  });
-});
